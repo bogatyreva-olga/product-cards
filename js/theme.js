5930e05332e9b4a8e62d1f803f4dc5c74ee23c3b
o.bogatyreva <bogatireva.o.v@gmail.com>
2023-06-08 07:57:43 +0300

    added persistence of the theme on page reload

--- js/theme.js
+++ js/theme.js
@@ -1,9 +1,42 @@
-export let toggleTheme = () => {
+const DARK_THEME_NAME = "dark-theme";
+const LIGHT_THEME_NAME = "light-theme";
+
+document.addEventListener("DOMContentLoaded", () => {
+    let theme = getCurrentTheme();
+    setTheme(theme);
+});
+
+let setTheme = (currentTheme) => {
+    document.body.classList.remove(DARK_THEME_NAME);
+    document.body.classList.remove(LIGHT_THEME_NAME);
+    document.body.classList.add(currentTheme);
+}
+
+let toggleTheme = () => {
     const btn = document.querySelector(".header__theme");
 
-    btn.addEventListener("click", function() {
-        document.body.classList.toggle('dark-theme');
+    btn.addEventListener("click", () => {
+        let currentTheme = getCurrentTheme();
+
+        document.body.classList.remove(currentTheme);
+
+        if (currentTheme === DARK_THEME_NAME) {
+            currentTheme = LIGHT_THEME_NAME;
+        } else {
+            currentTheme = DARK_THEME_NAME;
+        }
+        document.body.classList.add(currentTheme);
+
+        localStorage.setItem("current-theme", currentTheme);
     });
 }
 
+let getCurrentTheme = () => {
+    let currentTheme = localStorage.getItem("current-theme");
+    if (currentTheme === null) {
+        return DARK_THEME_NAME;
+    }
+    return currentTheme
+}
 
+export {toggleTheme}
