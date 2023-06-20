8a99be01448267e0f71d871b505068cf282d6db7
o.bogatyreva <bogatireva.o.v@gmail.com>
2023-06-20 23:03:01 +0300

    add media version for tablet screen

--- js/scroll-up.js
+++ js/scroll-up.js
@@ -15,7 +15,7 @@ const buttonScrollUp = {
             window.scrollTo({
                 top: 0,
                 left: 0,
-                behavior: 'smooth'
+                behavior: 'smooth',
             });
         }
     }
